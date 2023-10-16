3c199ab61630ca1a3726c250cf2bfdda63202688
Maria <86097040+mariafursevich@users.noreply.github.com>
2023-10-16 16:02:20 +0300

    Main.java

--- homework1/src/main/java/org/example/Main.java
+++ homework1/src/main/java/org/example/Main.java
@@ -1,7 +1,4 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
